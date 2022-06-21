28b54cb111542e07cbdf879a3a56bf3e94f1f75b
JuanK81 <jcasalsbadolato@gmail.com>
2022-06-21 12:29:20 +0200

    use assert.strictEqual

--- app.test.js
+++ app.test.js
@@ -1,11 +1,32 @@
-const { forEach } = require('./app');
+const assert = require('assert');
+const { forEach, map } = require('./app');
 
-let sum = 0;
-forEach([1,2,3], (value) => {
+const test = (desc, fn) => {
+  console.log('-----', desc);
+  
+  try {
+    fn();
+  } catch (err) {
+    console.log(err.message)
+  }
+};
+
+test('The forEach function', () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
     sum = sum + value;
+  });
+
+  assert.strictEqual(sum, 6, 'Expected summing array to be 6')
+  
 });
 
+test('The map function', () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
 
-if (sum !== 6) {
-    throw new Error('Expected summing array to be 6')
-}
+  assert.strictEqual(result[0], 2)
+  assert.strictEqual(result[1], 4)
+  assert.strictEqual(result[2], 6) 
+});
