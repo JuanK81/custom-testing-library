ac155ebdaa65554c4b7acd9c6c231d8b6bc94cc8
JuanK81 <jcasalsbadolato@gmail.com>
2022-06-21 12:36:44 +0200

    compare custom tests with mocha tests

--- app.test.js
+++ app.test.js
@@ -28,3 +28,23 @@ test('The map function', () => {
 
   assert.deepStrictEqual(result,[2, 4, 6]);
 });
+
+//try mocha
+
+it('The forEach function', () => {
+  let sum = 0;
+  forEach([1, 2, 3], (value) => {
+    sum = sum + value;
+  });
+
+  assert.strictEqual(sum, 6, 'Expected summing array to be 6')
+  
+});
+
+it('The map function', () => {
+  const result = map([1, 2, 3], (value) => {
+    return value * 2;
+  });
+
+  assert.deepStrictEqual(result,[2, 4, 6]);
+});
