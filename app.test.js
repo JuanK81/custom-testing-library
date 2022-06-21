86bece81a5be9b69166eb0ea1bdded8cbed1d652
JuanK81 <jcasalsbadolato@gmail.com>
2022-06-21 12:31:22 +0200

    use assert.deepStrictEqual

--- app.test.js
+++ app.test.js
@@ -26,7 +26,5 @@ test('The map function', () => {
     return value * 2;
   });
 
-  assert.strictEqual(result[0], 2)
-  assert.strictEqual(result[1], 4)
-  assert.strictEqual(result[2], 6) 
+  assert.deepStrictEqual(result,[2, 4, 6]);
 });
