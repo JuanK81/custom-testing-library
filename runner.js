ca46882082e9839aad16ad2a410e949dadd7f9d4
JuanK81 <jcasalsbadolato@gmail.com>
2022-06-22 12:36:57 +0200

    report errors

--- runner.js
+++ runner.js
@@ -2,33 +2,53 @@ const fs = require('fs');
 const path = require('path');
 
 class Runner {
-    constructor() {
-        this.testFiles = [];
-    }
+  constructor() {
+    this.testFiles = [];
+  }
 
-    async runTests() {
-        for (let file of this.testFiles) {
-            require(file.name);
+  async runTests() {
+    for (let file of this.testFiles) {
+      const beforeEaches = [];
+      global.beforeEach = (fn) => {
+        beforeEaches.push(fn);
+      };
+      global.it = (desc, fn) => {
+        beforeEaches.forEach((func) => func());
+        try {
+          fn();
+          console.log(`ok - ${desc}`);
+        } catch (err) {
+          console.log(`X - ${desc}`);
+          console.log('\t', err.message);
         }
+      };
+
+      try {
+        require(file.name);
+      } catch (err) {
+        console.log('X - error loading file',  file.name);
+        console.log('\t', err);
+      }
     }
+  }
 
-    async collectFiles(targetPath) {
-        // breath first
-        const files = await fs.promises.readdir(targetPath);
+  async collectFiles(targetPath) {
+    // breath first
+    const files = await fs.promises.readdir(targetPath);
 
-        for (let file of files) {
-            const filepath = path.join(targetPath, file);
-            const stats = await fs.promises.lstat(filepath);
+    for (let file of files) {
+      const filepath = path.join(targetPath, file);
+      const stats = await fs.promises.lstat(filepath);
 
-            if (stats.isFile() && file.includes('.test.js')) {
-                this.testFiles.push({ name: filepath });
-            } else if (stats.isDirectory()) {
-                const childFiles = await fs.promises.readdir(filepath);
+      if (stats.isFile() && file.includes('.test.js')) {
+        this.testFiles.push({ name: filepath });
+      } else if (stats.isDirectory()) {
+        const childFiles = await fs.promises.readdir(filepath);
 
-                files.push(...childFiles.map(f => path.join(file, f)));
-            }
-        }
+        files.push(...childFiles.map((f) => path.join(file, f)));
+      }
     }
+  }
 }
 
 module.exports = Runner;
--- sampleproject/test/forEach.test.js
+++ sampleproject/test/forEach.test.js
@@ -2,9 +2,14 @@ const assert = require('assert');
 
 const { forEach } = require('../index');
 
-it('should sum an array', () => {
-    const numbers = [1, 2, 3];
 
+let numbers;
+beforeEach(() => {
+    numbers = [1, 2, 3]
+});
+
+it('should sum an array', () => {
+    
     let total = 0;
 
     forEach(numbers, (value) => {
@@ -12,4 +17,12 @@ it('should sum an array', () => {
     });
 
     assert.strictEqual(total, 6);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+    numbers.push(3);
+});
+
+it('beforeEach is ran each time', () =>  {
+    assert.strictEqual(numbers.length, 3);
 });
