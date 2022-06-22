50fcfa4b1c7f0a6000d2af5db257537c3abba3d8
JuanK81 <jcasalsbadolato@gmail.com>
2022-06-22 13:37:07 +0200

    run dom in node with jsdom library

--- runner.js
+++ runner.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 const chalk = require('chalk');
+const render = require('./render');
+
+const forbiddenDirs = ['node_modules'];
 
 class Runner {
   constructor() {
@@ -8,20 +11,27 @@ class Runner {
   }
 
   async runTests() {
+    
     for (let file of this.testFiles) {
         console.log(chalk.grey(`------- ${file.shortName} -------`));
+
       const beforeEaches = [];
+
+      global.render = render;
+
       global.beforeEach = (fn) => {
         beforeEaches.push(fn);
       };
+
       global.it = (desc, fn) => {
         beforeEaches.forEach((func) => func());
         try {
           fn();
-          console.log(chalk.green(`ok - ${desc}`));
+          console.log(chalk.green(`\tOk - ${desc}`));
         } catch (err) {
-          console.log(chalk.red(`X - ${desc}`));
-          console.log(chalk.red('\t', err.message));
+            const message = err.message.replace(/\n/g, '\n\t\t');
+          console.log(chalk.red(`\tX - ${desc}`));
+          console.log(chalk.red('\t', message));
         }
       };
 
@@ -43,7 +53,7 @@ class Runner {
 
       if (stats.isFile() && file.includes('.test.js')) {
         this.testFiles.push({ name: filepath, shortName: file});
-      } else if (stats.isDirectory()) {
+      } else if (stats.isDirectory() && !file.includes(...forbiddenDirs)) {
         const childFiles = await fs.promises.readdir(filepath);
 
         files.push(...childFiles.map((f) => path.join(file, f)));
---
+++ samplewebproject/app.js
@@ -0,0 +1,12 @@
+document.querySelector('form').addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  const { value } = document.querySelector('input');
+
+  const header = document.querySelector('h1');
+  if (value.includes('@')) {
+    header.innerHTML = 'Valid Email'
+  } else {
+    header.innerHTML = 'Please enter a valid Email';
+  }
+});
